96460cb3b852e40fb2b8decbfb574297e93e6b1d
VITOR_4539 <vitor.fujihara@etec.sp.gov.br>
2025-09-17 12:42:43 -0300

    aaa

---
+++ src/ForAninhado.java
@@ -0,0 +1,14 @@
+public class ForAninhado {
+    public static void main(String[] args){
+        for (int i = 1; i < 5; i++){
+
+            for (int j = 0; j <= 5; j++)
+            {
+                System.out.printf("%d" , j + i);
+            }
+            System.out.println();
+        }
+
+
+    }
+}
